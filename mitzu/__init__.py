c16f814c797b08c1cbbab575965c4dadab9a938a
Istvan Meszaros <IstvanM@users.noreply.github.com>
2022-03-20 10:27:26 +0100

    Fixing notebook setup

--- mitzu/__init__.py
+++ mitzu/__init__.py
@@ -5,14 +5,21 @@ from mitzu.notebook.model_loader import DatasetModel
 import inspect
 from typing import Dict, Optional
 
-__all__ = ["init_project", "Connection", "ConnectionType"]
+__all__ = ["init_notebook_project", "Connection", "ConnectionType", "EventDataSource"]
 
 
-def find_notebook_globals() -> Optional[Dict]:
+Connection
+ConnectionType
+EventDataSource
+
+
+def _find_notebook_globals() -> Optional[Dict]:
     for stk in inspect.stack():
         parent_globals = stk[0].f_globals
-        if "init_notebook_project" in parent_globals:
+        if "init_notebook_project" in parent_globals and parent_globals != globals():
+            print("Found notebook context")
             return parent_globals
+    print("Couldn't find notebook context")
     return None
 
 
@@ -23,5 +30,8 @@ def init_notebook_project(
     glbs=None,
 ) -> DatasetModel:
     if glbs is None:
-        glbs = find_notebook_globals()
-    return P.init_project(source=source, start_dt=start_dt, end_dt=end_dt, glbs=glbs)
+        glbs = _find_notebook_globals()
+    print("Initializing project ...")
+    res = P.init_project(source=source, start_dt=start_dt, end_dt=end_dt, glbs=glbs)
+    print("Finished project initialization")
+    return res
