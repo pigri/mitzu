1e8af5ec358d6b763077da92fe310bd370cf14d8
istvanmeszaros <istvan.meszaros.88@gmail.com>
2022-10-10 11:14:23 +0200

    version bump and small fix for dashboard

--- mitzu/notebook/dashboard.py
+++ mitzu/notebook/dashboard.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import os
 import sys
 from random import random
+from typing import Optional
 
 import dash_bootstrap_components as dbc
 import flask
@@ -15,7 +16,9 @@ from mitzu.notebook.component import CSS
 LOG_HANDLER = sys.stdout
 
 
-def dashboard(mode: str = "inline"):
+def dashboard(
+    mode: str = "inline", port: Optional[int] = None, host: Optional[str] = None
+):
     app = JupyterDash(
         __name__,
         compress=True,
@@ -39,7 +42,15 @@ def dashboard(mode: str = "inline"):
         app=app,
         authorizer=AUTH.GuestMitzuAuthorizer(),
     )
+    if port:
+        os.environ["PORT"] = str(port)
+    else:
+        os.environ["PORT"] = str(18000 + int(random() * 10000))
 
-    os.environ["PORT"] = str(18000 + int(random() * 10000))
+    if host:
+        os.environ["HOST"] = host
+    else:
+        os.environ["HOST"] = "0.0.0.0"
+
     webapp.init_app()
     app.run_server(mode=mode)
