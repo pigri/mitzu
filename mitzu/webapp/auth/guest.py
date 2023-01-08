570f27a6c376fe951f7983fa83b8a3c5d6d05ef5
Akos Adam Medgyes <akos.adam.medgyes@gmail.com>
2023-01-08 16:30:05 +0100

    Remove unused GuestAuthorizer

--- mitzu/webapp/auth/guest.py
+++
@@ -1,7 +0,0 @@
-from typing import Optional
-from mitzu.webapp.auth.authorizer import MitzuAuthorizer
-
-
-class GuestAuthorizer(MitzuAuthorizer):
-    def get_user_email(self, _: str) -> Optional[str]:
-        return "Guest"
